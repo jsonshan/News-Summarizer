a542403ed1b77eff6dc7c4dfcd221d27bef8925b
Kelvin Chen <kelvin@kelvins-mbp-3.mynetworksettings.com>
2024-11-09 16:57:13 -0500

    changes to genreSelection

--- client/src/components/GenreSelection.jsx
+++ client/src/components/GenreSelection.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
+
+import React, {useEffect} from 'react'
 import "../styles/GenreSelection.css"
 
 
 function GenreSelection({genres}) {
-
-
-  function select(genreName){
+  function select(genreName) {
     genres.forEach((element) => {
       let item = document.getElementById(element);
       let x = document.getElementById(genreName);
-      if(element === genreName) x.classList.add("enabled");
+      if (element === genreName) x.classList.add("enabled");
       else item.classList.remove("enabled");
-    })
+    });
   }
-  
-  // function selected(element){
-  //   let item = document.getElementById(element);
-  //   item.classList.add("enabled");
-  // }
+
+  useEffect(() => {
+    // Set "finance" as the initial genre with the "enabled" class
+    if (genres.includes("finance")) {
+      select("finance");
+    }
+  }, [genres]); // Only runs when 'genres' changes
 
   return (
     <div className="genre-container">
       <div className="genre-wrapper">
         {genres.map((i, j) => {
-          return <div className="genre-cards" id = {i} key={j} onClick = {() => {select(i)}}>{i}</div>
+          return <div className="genre-cards" id = {i} key={j} onClick = {() => {select(i)}}> {i}  
+          </div>
         })}
       </div>
     </div>
