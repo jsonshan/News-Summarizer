6d610e27b5ea829f83f55ddee5bd9fa2e46ba834
Sam Lee <sam.lee03@myhunter.cuny.edu>
2024-11-09 11:56:34 -0500

    add styles to components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import TextBlock from './components/TextBlock'
+import NewsIcon from './components/NewsIcon'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +9,8 @@ function App() {
   return (
     <>
       <div className='App'>
-
+        <TextBlock type="main-summary"/>
+        <TextBlock type="sub-summary" logo='https://yt3.googleusercontent.com/n5DRh94eycw6xGcOKTn6LKQwztTwaw24fXPniFTXA3VPgwJaiOFdBwJNtXRHYUf7OdEAk9upwH0=s900-c-k-c0x00ffffff-no-rj' srcName="CNN"/>
       </div>
     </>
   )
--- client/src/components/TextBlock.jsx
+++ client/src/components/TextBlock.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
-function TextBlock() {
+import "../styles/TextBlock.css"
+function TextBlock({type, logo, srcName}) {
   return (
-    <div>TextBlock</div>
+    <>
+      <div className={`${type} text-block`}>
+      {
+        (type == 'sub-summary'
+        ? 
+        <>
+          <div class="item-display">
+            <img src={logo}/>
+            <p>{srcName}</p>
+          </div>
+          <div class="divider"></div>
+        </>
+        :
+          <></>
+        )
+      }
+        <div className="text-section">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A ratione laborum quia eaque consequuntur odit ex, tempore alias eius illo inventore amet corrupti placeat nisi quibusdam recusandae rem pariatur veniam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolor nihil enim dicta explicabo repellat consequatur, quo blanditiis soluta molestias odit beatae porro, corporis in distinctio sapiente vel perferendis ipsa.</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A ratione laborum quia eaque consequuntur odit ex, tempore alias eius illo inventore amet corrupti placeat nisi quibusdam recusandae rem pariatur veniam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolor nihil enim dicta explicabo repellat consequatur, quo blanditiis soluta molestias odit beatae porro, corporis in distinctio sapiente vel perferendis ipsa.</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A ratione laborum quia eaque consequuntur odit ex, tempore alias eius illo inventore amet corrupti placeat nisi quibusdam recusandae rem pariatur veniam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolor nihil enim dicta explicabo repellat consequatur, quo blanditiis soluta molestias odit beatae porro, corporis in distinctio sapiente vel perferendis ipsa.</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A ratione laborum quia eaque consequuntur odit ex, tempore alias eius illo inventore amet corrupti placeat nisi quibusdam recusandae rem pariatur veniam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolor nihil enim dicta explicabo repellat consequatur, quo blanditiis soluta molestias odit beatae porro, corporis in distinctio sapiente vel perferendis ipsa.</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A ratione laborum quia eaque consequuntur odit ex, tempore alias eius illo inventore amet corrupti placeat nisi quibusdam recusandae rem pariatur veniam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dolor nihil enim dicta explicabo repellat consequatur, quo blanditiis soluta molestias odit beatae porro, corporis in distinctio sapiente vel perferendis ipsa.</p>
+        </div>
+      </div>
+    </>
   )
 }
 
