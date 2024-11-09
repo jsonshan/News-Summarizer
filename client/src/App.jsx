fb3ce635b005b6d2f05a6aefffbdd44f357a451b
Sam Lee <sam.lee03@myhunter.cuny.edu>
2024-11-09 17:42:22 -0500

    m

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import './App.css'
 import TextBlock from './components/TextBlock'
 import NewsSource from './components/NewsSource'
@@ -8,21 +8,22 @@ import GenreSelection from './components/GenreSelection'
 function App() {
   const [sources, setSources] = useState([['https://yt3.googleusercontent.com/n5DRh94eycw6xGcOKTn6LKQwztTwaw24fXPniFTXA3VPgwJaiOFdBwJNtXRHYUf7OdEAk9upwH0=s900-c-k-c0x00ffffff-no-rj', 'CNN'], ['https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Fox_News_Channel_logo.svg/1280px-Fox_News_Channel_logo.svg.png', 'Fox News']])
   const [selectedPage, setSelectedPage] = useState('technology');
-  const [genres, setGeneres] = useState(['Finance', 'Environment', 'Technology']);
+  const [genres, setGeneres] = useState(['Finance', 'Politics', 'World', 'Health', 'Business', 'Environment', 'Tech', 'Entertainment', 'Science', 'History', 'Sports']);
 
   const[selectedSources, setSelectedSources] = useState([true, true])
   const [selectedGenre, setSelectedGenre] = useState('Finance');
   const updateSources = (data) => {
     setSelectedSources(data);
-    console.log("new sources ", data);
+    // console.log("new sources ", data);
   }
   const handleChangeGenre = (genre) => {
     setSelectedGenre(genre);
-    console.log("new genre  ", genre);
+    // console.log("new genre  ", genre);
   }
+  useEffect(() => {
+    console.log('Updated selectedSources:', selectedSources);
+  }, [selectedSources, selectedGenre]);
 
-
-  
   return (
     <>
       <div className='App'> {/* main page */}
